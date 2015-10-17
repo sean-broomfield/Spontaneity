57cf3fc24c0e2b36a2656bd28342f1809f94b0a6
seanbroomfield <srbroomfield@aol.com>
2015-10-17 16:33:38 -0400

    commit.

---
+++ src/com/example/Spontaneity/Login_Activity.java
@@ -0,0 +1,16 @@
+package com.example.Spontaneity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login_Activity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(android.R.style.Theme_Holo_Light_NoActionBar);
+        setContentView(R.layout.main);
+    }
+}
